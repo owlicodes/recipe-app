5a4dec909ef5e30e59aff4b6f91ed8fe313cdb2e
Jose Diago <owlicodes@gmail.com>
2024-11-11 12:25:34 +0800

    Fixing eslint build errors.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <head>
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📦</text></svg>"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👩‍🍳</text></svg>"
         />
       </head>
       <body className="antialiased">
